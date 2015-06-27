c44752fa34ae432c70f2d02be600235820cff9b9
TuckerWatts <tucker@tuckerwatts.com>
2015-06-27 18:44:42 -0400

    STERILIZER Actually just added serializer gem, model, controllers, routes

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,27 @@
+class LevelsController < ApplicationController
+
+  def new
+    @level = Level.create(properties: params[:properties])
+    if @post.save
+    render json: { level: @level.as_json(only: [:properties]) },
+           status: :created
+    else
+      render json: { errors: @level.errors.full_messages },
+             status: :unprocessable_entity
+    end
+
+  end
+
+  def show
+    @level = Level.find(params[:id])
+    render json: @level
+  end
+
+  def index
+    @level = Level.all
+    render json: { level: @level.as_json(only: [:properties]) },
+        status: :ok
+  end
+
+end
+
