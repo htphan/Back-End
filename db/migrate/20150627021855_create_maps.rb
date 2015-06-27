6b7fd3c73722f0481caf724ed6f789a1b255ccb2
Henry Phan <hphan.cs@gmail.com>
2015-06-26 22:19:11 -0400

    Add Map Controller

---
+++ db/migrate/20150627021855_create_maps.rb
@@ -0,0 +1,10 @@
+class CreateMaps < ActiveRecord::Migration
+  def change
+    create_table :maps do |t|
+      t.integer :budget
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
