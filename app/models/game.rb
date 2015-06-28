ad02f7084dd3da21517eff48ad79a472c1d448d4
Anthony Franco <anthony_franco@alumni.brown.edu>
2015-06-28 10:12:44 -0400

    Fixed validations so that the user can play the same game multiple times

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
 	belongs_to :map
 
   validates :level_id, presence: true 
-  validates :username, uniqueness: true, presence: true
+  validates :username, presence: true
   validates :score, presence: true
 end
