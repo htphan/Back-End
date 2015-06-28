86f5cd7452cd14b22a3fa7ed41d3c1bb1097fd3c
TuckerWatts <tucker@tuckerwatts.com>
2015-06-27 21:32:32 -0400

    added error msgs to show and index methods in Level controller

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -15,13 +15,24 @@ class LevelsController < ApplicationController
   def show
     @level = Level.find(params[:id])
     properties = eval(@level.properties)
-    render json: { level: {id: @level.id, properties: properties} }
+    if @level
+    render json: { level: {id: @level.id, properties: properties} },
+           status: :ok
+    else
+      render json: { errors: @level.errors.full_messages },
+             status: :bad_request
+    end
   end
 
   def index
     @level = Level.all
+    if @level
     render json: { level: @level.as_json(only: [:properties]) },
         status: :ok
+    else
+      render json: { errors: @level.errors.full_messages },
+             status: :bad_request
+    end
   end
 
 end
