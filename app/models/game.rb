fd0f69b6d4b7c8818bbf05359edc70f10a524450
Anthony Franco <anthony_franco@alumni.brown.edu>
2015-06-28 10:10:08 -0400

    added validations to games model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ActiveRecord::Base
 	belongs_to :map
+
+  validates :level_id, presence: true 
+  validates :username, uniqueness: true, presence: true
+  validates :score, presence: true
 end
